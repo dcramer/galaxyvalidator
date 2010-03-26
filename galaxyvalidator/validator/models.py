94d46e20c2be5e7149a5857ba38e7458ac865ad6
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-03-25 19:39:34 -0500

    attempt different piping

--- galaxyvalidator/validator/models.py
+++ galaxyvalidator/validator/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 
 from validator.fields import *
 
+from cStringIO import StringIO
+
 import datetime
 import subprocess
 
@@ -15,8 +17,8 @@ class Result(models.Model):
     def process(self):
         args = [settings.LAPIN_BINARY_PATH, '-I', settings.LAPIN_INCLUDE_PATH, '-']
         print args
-        p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-        results = p.communicate(self.input)[0]
+        p = subprocess.Popen(args, stdin=StringIO(subprocess.PIPE), stdout=subprocess.PIPE, shell=True)
+        results = p.communicate()[0]
         self.output = results
         print results
         
