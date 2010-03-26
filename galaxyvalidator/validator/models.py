67415a6312122b3aec54be8712fbb8591910a7f3
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-03-25 20:52:50 -0500

    tweak results

--- galaxyvalidator/validator/models.py
+++ galaxyvalidator/validator/models.py
@@ -3,8 +3,6 @@ from django.conf import settings
 
 from fields import *
 
-from StringIO import StringIO
-
 import datetime
 import subprocess
 
@@ -19,9 +17,9 @@ class Result(models.Model):
     
     def process(self):
         args = [settings.LAPIN_BINARY_PATH, '-I', settings.LAPIN_INCLUDE_PATH, '-']
-        p = subprocess.Popen(args, stdin=self.input, stdout=subprocess.PIPE)
+        p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         
-        results = p.communicate()
+        results = p.communicate(input=self.input)
         if results[1]:
             raise LapinError(results[1])
 
