8e34f709bbf9c60ac9faf7f0c8557b8c15c434ec
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-03-26 13:46:23 -0500

    show code

--- galaxyvalidator/validator/forms.py
+++ galaxyvalidator/validator/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ValidateTextForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs=dict(rows=20, cols=100)))
+    text = forms.CharField(widget=forms.Textarea(attrs=dict(rows=15, cols=100)))
 
 class ValidateFileForm(forms.Form):
     file = forms.FileField()
